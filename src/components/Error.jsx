bf68b927235605891529ec42d83e3223d5de9807
nziokikevin <kevinnzioki10@gmail.com>
2023-05-10 14:51:04 +0300

    :lipstick: :sparkles: feat: designing and configuring error page

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const Error = () => (
-  <div>Error</div>
+  <div className="flex flex-col w-full justify-center items-center">
+    <h1 className="text-white font-bold text-2xl mt-2">Something went wrong. Please try again.</h1>
+  </div>
 );
 
 export default Error;
