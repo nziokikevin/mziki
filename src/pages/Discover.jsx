56450ab8d1d69d804b51be7b1411b4876dea2a1d
nziokikevin <kevinnzioki10@gmail.com>
2023-05-10 14:49:39 +0300

    :art: :sparkles: feat: displaying tracks on discover page

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,8 +1,14 @@
-import { genres } from "../assets/constants";
-import SongCard from "../components/SongCard";
+import { genres } from '../assets/constants';
+import { SongCard, Loader, Error } from '../components';
+import { useGetTopChartsQuery } from '../redux/services/shazamCore';
 
 const Discover = () => {
-  const genreTitle = "Pop";
+  const { data, isFetching, error } = useGetTopChartsQuery([]);
+  const genreTitle = 'Pop';
+
+  if (isFetching) return <Loader title="Loading songs..." />;
+
+  if (error) return <Error />;
 
   return (
     <div className="flex flex-col">
@@ -24,7 +30,7 @@ const Discover = () => {
       </div>
 
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((song, i) => (
+        {data.tracks.map((song, i) => (
           <SongCard key={song.key} song={song} i={i} />
         ))}
       </div>
