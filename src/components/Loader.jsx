72023a8fbd0a4385fde63a516100b392160b2eb6
nziokikevin <kevinnzioki10@gmail.com>
2023-05-10 14:50:02 +0300

    :sparkles: feat: configuring the loading page

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,5 +1,10 @@
-const Loader = () => (
-  <div>Loader</div>
+import { loader } from '../assets';
+
+const Loader = ({ title }) => (
+  <div className="flex flex-col justify-center items-center w-full">
+    <img src={loader} alt="loader" className="h-32 w-32 object-contain" />
+    <h1 className="font-bold text-white text-2xl mt-2">{title || 'Loading...'}</h1>
+  </div>
 );
 
 export default Loader;
