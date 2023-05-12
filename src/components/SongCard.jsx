4643041b57cf8e13afdb97bbd7f8fdc7905f563f
nziokikevin <kevinnzioki10@gmail.com>
2023-05-12 23:56:41 +0300

    :construction: project: work in progress

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -1,5 +1,24 @@
-const SongCard = () => (
-  <div>SongCard</div>
-);
+import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import PlayPause from './PlayPause';
+import { playPause, setActiveSong } from '../redux/features/playerSlice';
+
+const SongCard = ({ song, i }) => {
+  const activeSong = 'Test';
+
+  return (
+    <div className="flex flex-col bg-white/5 bg-opactiy-80 w-[250px] backdrop-blur-sm animate-slideup rounded-lg cursor-pointer p-4">
+      <div className="w-full h-56 group relative">
+        <div
+          className={`bg-black bg-opacity-50 absolute justify-center items-center inset-0 group-hover:flex ${
+            activeSong?.title === song.title
+              ? 'flex bg-black bg-opacity-70'
+              : 'hidden'
+          }`}
+        ></div>
+      </div>
+    </div>
+  );
+};
 
 export default SongCard;
