a85f0655a70f3f879c8b643a1e22d193e9cc0bd3
nziokikevin <kevinnzioki10@gmail.com>
2023-05-13 12:18:29 +0300

    :sparkles: feat: adding the play-pause functionality

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,8 +1,11 @@
+import { useDispatch, useSelector } from 'react-redux';
 import { genres } from '../assets/constants';
 import { SongCard, Loader, Error } from '../components';
 import { useGetTopChartsQuery } from '../redux/services/shazamCore';
 
 const Discover = () => {
+  const dispatch = useDispatch();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
   const { data, isFetching, error } = useGetTopChartsQuery([]);
   const genreTitle = 'Pop';
 
@@ -31,7 +34,7 @@ const Discover = () => {
 
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
         {data.tracks.map((song, i) => (
-          <SongCard key={song.key} song={song} i={i} />
+          <SongCard key={song.key} song={song} i={i} isPlaying={isPlaying} activeSong={activeSong} data={data} />
         ))}
       </div>
     </div>
